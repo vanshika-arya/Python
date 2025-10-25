24ad610ab298365fd13e7ff457a3df016747a3ec
Vanshika Arya <vanshika3915@gmail.com>
2025-10-25 11:50:22 +0530

    Add files via upload

---
+++ Measurement.py
@@ -0,0 +1,10 @@
+inches = float(input())
+foot = inches / 12.0
+yard = inches / 36.0
+centimetres = inches * 2.54
+meter = inches / 39.37
+
+print("Foot:", foot)
+print("Yard:", yard)
+print("Centimetres:", centimetres)
+print("Meter:", meter)
