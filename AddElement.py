9118880a7eb9a4979a48470dfbfa47157b2fb17f
Vanshika Arya <vanshika3915@gmail.com>
2025-11-11 19:02:35 +0530

    Add files via upload

---
+++ AddElement.py
@@ -0,0 +1,12 @@
+#single value add()
+s={1,2,3}
+s.add(0)
+print(s)
+
+#add multiple value update() -->assending order me value add hongi
+s.update([6,8,9])
+print(s)
+
+#add list and set
+s.update([67,45,23],{87,90})
+print(s)
