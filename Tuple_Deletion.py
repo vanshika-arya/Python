6982ece11a13c624eb329723d6e1c744a72ea3c4
Vanshika Arya <vanshika3915@gmail.com>
2025-11-24 20:22:44 +0530

    Add files via upload

---
+++ Tuple_Deletion.py
@@ -0,0 +1,27 @@
+t=(1,2,3,4,5,1,3,1,4,16)
+print(t)
+#delete entire tuple using del keyword
+# del t
+print(t)
+
+#tuple count
+print(t.count(1))
+
+#tuple index
+print(t.index(3))
+
+#tuple Membership
+print(1 in t)
+print(7 in t)
+print(len(t))
+
+#sorting tuple
+new_t=sorted(t)
+print(new_t)
+
+#maximum and minimum element of tuple
+
+print(max(t))
+print(min(t))
+
+print(sum(t))
