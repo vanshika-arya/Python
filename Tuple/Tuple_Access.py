ab2334c40f414df555d9d192eb7e3f5824e530ad
Vanshika Arya <vanshika3915@gmail.com>
2025-11-24 20:19:23 +0530

    Add files via upload

---
+++ Tuple/Tuple_Access.py
@@ -0,0 +1,7 @@
+t="vanshi", (67,23.9,3),"v"
+print(t[0])
+
+print(t[-1])
+print(t[1])
+
+print(t[1][2])
---
+++ Tuple/Tuple_Creation.py
@@ -0,0 +1,25 @@
+#empty tuple
+t=()
+print(t)
+
+#tuple having integers
+t=(1,2,3)
+print(t)
+
+#tuple with mixed datatypes
+t =(1,'vanshika',28.9,'alshi')
+print(t)
+
+#nested tuple
+t=(1,(2,3,4),[1,'raju',28])
+print(t)
+
+t=('vanshi')
+print(type(t))
+
+t=('vanshi',) #tuple me jab tak mutlple values nhi denge tab tak uska type use as a tuple treat nhi karega...multple values dene hone 
+print(type(t))
+
+#parenthesis is optional
+t= "vanshi",45,78.9
+print(type(t))
