928cb6c435b942b9c1668ab63fc5ead821a54ccc
Vanshika Arya <vanshika3915@gmail.com>
2025-11-25 20:36:59 +0530

    Add files via upload

---
+++ Dict_Creation.py
@@ -0,0 +1,20 @@
+#dictinary me keys and values hoti hu pair me...speed fast hoti hai
+#empty dictionary
+my_dict ={}
+print(my_dict)
+
+#dictionary with integer keys
+my_dict= {1: 'abc', 2:'xyz'}
+print(my_dict)
+
+#dictionay with mixed keys
+my_dict ={'name': 'vanshi' , 1:['abc', 'xyz']}
+print(my_dict)
+
+#create  dict usng dict() method
+my_dict = dict()
+
+#list--->dictionary
+my_dict = dict([(1, 'abc'),(2,'xyz')])
+print(my_dict)
+
