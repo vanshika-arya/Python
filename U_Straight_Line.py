42cbc74c02e14c51ae5db309acd4bfd5ae29de4b
Vanshika Arya <vanshika3915@gmail.com>
2026-02-03 20:29:40 +0530

    line

---
+++ U_Straight_Line.py
@@ -0,0 +1,7 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+if x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2) == 0:
+    print("YES")
+else:
+    print("NO")
